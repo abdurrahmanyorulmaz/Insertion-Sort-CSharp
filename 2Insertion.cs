f6807f142846b447dbe05c141f1cc505607b9d3d
axyorulmaz <49240098+axyorulmaz@users.noreply.github.com>
2020-12-12 13:53:27 +0300

    Add files via upload

---
+++ 2Insertion.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace ConsoleApp3
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Insertion_Sort_Abdurrahman_YORULMAZ");
+			int elements;
+			Console.WriteLine("Write how many elements you want to enter the array:");
+			elements = Convert.ToInt32(Console.ReadLine());
+			int[] array = new int[elements];
+			Console.WriteLine("\nEnter " + elements + " numbers.\n");
+			for (int counter = 0; counter < elements; counter++)
+			{
+				Console.WriteLine("Enter the element to be kept in the " + counter + ". Index");
+				array[counter] = Convert.ToInt32(Console.ReadLine());
+			}
+			Console.Write("\n\nThe first version of the series: ");
+			foreach (int value in array)
+			{
+				Console.Write(value + " ");
+			}
+			Console.WriteLine();
+			sort(array);
+			write(array);
+		}
+		public static void write(int[] array)
+		{
+			Console.Write("The series in ascending order: ");
+			foreach (int value in array)
+			{
+				Console.Write(value + " ");
+			}
+			Console.WriteLine("\n\n>>Press any key to exit.");
+			Console.ReadKey();
+		}
+		public static void sort(int[] array)
+		{
+			int holder;
+			for (int j = 1; j < array.Length; j++)
+			{
+				int holder = array[j];
+				int i = j - 1;
+				while (i >= 0 && array[i] > holder)
+				{
+					array[i + 1] = array[i];
+					i = i - 1;
+				}
+				array[i + 1] = holder;
+			}
+		}
+	}
+}
